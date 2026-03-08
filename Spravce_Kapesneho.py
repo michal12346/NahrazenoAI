2a3870d1fa1f5444b732347161cf7c729a8cbccf
michal12346 <michal.balon@tria-tr.cz>
2026-03-08 13:55:05 +0100

    Přidán výpočet průměru a kontrola překročení limitu

--- Spravce_Kapesneho.py
+++ Spravce_Kapesneho.py
@@ -1,23 +1,28 @@
-# Program na hlidani penez - 1 verze
-print("-----------------------------------------")
-print("Vitejte v programu Spravce kapesneho")
-print("-----------------------------------------")
-
-# Tady si nastavim kolik mam penez na zacatku
+# Program na hlidani penez - 2 verze (pridana logika)
 nasetreno = float(input("Zadej, kolik mas celkem penez: "))
 limit = float(input("Zadej svuj tydenni limit na utratu: "))
 
-# Seznam vydaju - zatim jen tri polozky at to neni slozity
 print("Zadej nyni svoje posledni tri nakupy:")
 jidlo = float(input("1. Kolik jsi dal za jidlo: "))
 zabava = float(input("2. Kolik jsi dal za zabavu: "))
 ostatni = float(input("3. Kolik jsi dal za ostatni veci: "))
 
-# Zakladni vypocet celkove utraty
 celkova_utrata = jidlo + zabava + ostatni
+zbytek = nasetreno - celkova_utrata
 
-print("-----------------------------------------")
-print("Zadal jsi vsechny udaje.")
-print("Tvoje aktualni utrata je zatim:", celkova_utrata)
-print("V penezence ti zbyva:", nasetreno - celkova_utrata)
-print("-----------------------------------------")
+# Tady kontroluju jestli uzivatel neni v minusu
+if zbytek < 0:
+    print("POZOR: Jsi v minusu! Musis vic setrit.")
+    print("Chybi ti presne:", abs(zbytek), "Kc.")
+else:
+    print("Jsi v pohode, jeste mas:", zbytek, "Kc.")
+
+# Vypocet prumeru na jednu polozku
+prumer = celkova_utrata / 3
+print("Prumerne za jednu vec utratis:", prumer)
+
+# Kontrola tydenniho limitu
+if celkova_utrata > limit:
+    print("VAROVANI: Prekrocil jsi svuj tydenni limit!")
+else:
+    print("Limit jsi neprekrocil.")
