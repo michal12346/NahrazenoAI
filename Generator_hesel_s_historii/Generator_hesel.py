9e8e989979ac3bd98a9af0549582fcc5922b63c5
michal12346 <michal.balon@tria-tr.cz>
2026-03-17 20:45:01 +0100

    Pridani zakladniho generatoru hesel

--- Generator_hesel_s_historii/Generator_hesel.py
+++ Generator_hesel_s_historii/Generator_hesel.py
@@ -3,13 +3,12 @@
 import secrets
 import string
 
-# 2) Generátor hesla
-
+# Generátor hesla
 def generate_password(length=12):
     chars = string.ascii_letters + string.digits + "!@#$%^&*_-+"
     return "".join(secrets.choice(chars) for _ in range(length))
 
-# 5) Hlavní program
-# vygeneruje heslo
+# Hlavní program
+# Vygeneruje heslo
 heslo = generate_password(16)
 print("Vygenerované heslo:", heslo)
