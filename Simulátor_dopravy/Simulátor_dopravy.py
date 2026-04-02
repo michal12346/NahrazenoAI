c83beda5a47c674e7109b67bf5ec1a3b8eb9c537
michal12346 <michal.balon@tria-tr.cz>
2026-04-02 21:49:34 +0200

    Přidáno sčítání autobusů a detekce dopravní zácpy

--- Simulátor_dopravy/Simulátor_dopravy.py
+++ Simulátor_dopravy/Simulátor_dopravy.py
@@ -1,15 +1,26 @@
-# Simulátor dopravy ve městě - VERZE 1
-
+# Simulátor dopravy ve městě - VERZE 2
 celkem_aut = 0
+celkem_autobusu = 0
 pokracovat = "ano"
 
-# SMYČKA: Hlavní cyklus programu, který se opakuje, dokud uživatel zadává "ano"
+# SMYČKA: Hlavní cyklus pro průběžné zadávání dat
 while pokracovat == "ano":
-    pocet_aut = int(input("Kolik aut právě projelo křižovatkou? "))
+    pocet_aut = int(input("Kolik aut právě projelo? "))
+    pocet_autobusu = int(input("Kolik autobusů právě projelo? "))
     
     celkem_aut = celkem_aut + pocet_aut
-    print("Celkem už projelo:", celkem_aut, "aut.")
+    celkem_autobusu = celkem_autobusu + pocet_autobusu
     
+    # LOGIKA: Výpočet vytížení křižovatky. 
+    # Autobus zabírá více místa, proto se násobí třemi.
+    zatizeni = pocet_aut + (pocet_autobusu * 3)
+    
+    # LOGIKA: Rozhodnutí, zda vzniká zácpa
+    if zatizeni > 20:
+        print("POZOR: Tvoří se dopravní zácpa!")
+    else:
+        print("Doprava je plynulá.")
+        
     pokracovat = input("Chceš zadat další měření? (ano/ne): ")
 
-print("Simulace byla ukončena.")
+print("Konec simulace. Celkem projelo", celkem_aut, "aut a", celkem_autobusu, "autobusů.")
