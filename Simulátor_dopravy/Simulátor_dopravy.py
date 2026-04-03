080fda195a062d8cdc4cc43f51cdb4c2232dc803
michal12346 <michal.balon@tria-tr.cz>
2026-04-03 21:41:33 +0200

    Přidáno počítání průměru a vylepšeno ukončování programu

--- Simulátor_dopravy/Simulátor_dopravy.py
+++ Simulátor_dopravy/Simulátor_dopravy.py
@@ -1,26 +1,36 @@
-# Simulátor dopravy ve městě - VERZE 2
+# Simulátor dopravy ve městě - FINÁLNÍ VERZE
 celkem_aut = 0
 celkem_autobusu = 0
-pokracovat = "ano"
+pocet_mereni = 0 
 
-# SMYČKA: Hlavní cyklus pro průběžné zadávání dat
-while pokracovat == "ano":
+print("Vítejte v Simulátoru městské dopravy!")
+
+# SMYČKA: Nekonečný cyklus, přeruší se až příkazem 'break'
+while True:
     pocet_aut = int(input("Kolik aut právě projelo? "))
     pocet_autobusu = int(input("Kolik autobusů právě projelo? "))
     
     celkem_aut = celkem_aut + pocet_aut
     celkem_autobusu = celkem_autobusu + pocet_autobusu
+    pocet_mereni = pocet_mereni + 1  
     
-    # LOGIKA: Výpočet vytížení křižovatky. 
-    # Autobus zabírá více místa, proto se násobí třemi.
     zatizeni = pocet_aut + (pocet_autobusu * 3)
     
-    # LOGIKA: Rozhodnutí, zda vzniká zácpa
     if zatizeni > 20:
         print("POZOR: Tvoří se dopravní zácpa!")
     else:
         print("Doprava je plynulá.")
         
-    pokracovat = input("Chceš zadat další měření? (ano/ne): ")
+    odpoved = input("Chceš pokračovat? (napiš 'konec' pro zastavení, nebo cokoliv pro pokračování): ")
+    # LOGIKA: Ukončení smyčky, pokud uživatel zadá klíčové slovo
+    if odpoved == "konec":
+        break  
 
-print("Konec simulace. Celkem projelo", celkem_aut, "aut a", celkem_autobusu, "autobusů.")
+print("\n--- KONEČNÉ STATISTIKY ---")
+print("Celkem aut:", celkem_aut)
+print("Celkem autobusů:", celkem_autobusu)
+
+# LOGIKA: Ochrana proti chybě dělení nulou, pokud by uživatel ukončil program hned
+if pocet_mereni > 0:
+    prumer = celkem_aut / pocet_mereni
+    print("Průměrně projelo", prumer, "aut během jednoho měření.")
