5f0c8dc78370c0aa3a68d669b04898aec4aa9e7f
michal12346 <michal.balon@tria-tr.cz>
2026-04-08 17:27:07 +0200

    Vytvořen základní skript a přidány seznamy slov
    Initial version of the random story generator with character, location, and action lists.

---
+++ Generátor_náhodných_příběhů/Generátor_náhodných_příběhů.py
@@ -0,0 +1,13 @@
+# Generátor náhodných příběhů - PRVNÍ VERZE
+
+# Vytvoření proměnné 'postavy', do které uložíme seznam (list) textových řetězců (jmen postav)
+postavy = ["Rytíř", "Drak", "Princezna", "Čaroděj"]
+
+# Vytvoření proměnné 'mista', do které uložíme seznam různých lokací
+mista = ["v temném lese", "na starém hradě", "v kouzelné jeskyni", "na létajícím ostrově"]
+
+# Vytvoření proměnné 'deje', do které uložíme seznam činností, které postavy dělají
+deje = ["našel skrytý poklad.", "bojoval se strašlivou nestvůrou.", "usnul hlubokým spánkem.", "objevil tajemný portál."]
+
+# Prozatím jen vypíšeme testovací zprávu, abychom viděli, že program běží
+print("Seznamy slov byly úspěšně načteny.")
