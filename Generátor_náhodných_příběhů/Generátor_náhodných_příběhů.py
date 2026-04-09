d1749572fc2cbca8fafdb71ab2829cd3db32d79c
michal12346 <michal.balon@tria-tr.cz>
2026-04-09 17:43:38 +0200

    Přidán modul random a implementován náhodný výběr slov do jedné věty

--- Generátor_náhodných_příběhů/Generátor_náhodných_příběhů.py
+++ Generátor_náhodných_příběhů/Generátor_náhodných_příběhů.py
@@ -1,4 +1,7 @@
-# Generátor náhodných příběhů - PRVNÍ VERZE
+# Generator nahodných příběhů - DRUHÁ VERZE
+
+# Importujeme vestavěnou knihovnu 'random', která nám umožní vybírat náhodné prvky
+import random 
 
 # Vytvoření proměnné 'postavy', do které uložíme seznam (list) textových řetězců (jmen postav)
 postavy = ["Rytíř", "Drak", "Princezna", "Čaroděj"]
@@ -9,5 +12,18 @@ mista = ["v temném lese", "na starém hradě", "v kouzelné jeskyni", "na léta
 # Vytvoření proměnné 'deje', do které uložíme seznam činností, které postavy dělají
 deje = ["našel skrytý poklad.", "bojoval se strašlivou nestvůrou.", "usnul hlubokým spánkem.", "objevil tajemný portál."]
 
-# Prozatím jen vypíšeme testovací zprávu, abychom viděli, že program běží
-print("Seznamy slov byly úspěšně načteny.")
+# Funkce random.choice() vezme náš seznam 'postavy' a vybere z něj jeden náhodný prvek, ten uloží do nové proměnné
+vybrana_postava = random.choice(postavy)
+
+# To samé uděláme pro místo
+vybrane_misto = random.choice(mista)
+
+# A to samé uděláme pro děj
+vybrany_dej = random.choice(deje)
+
+# Spojíme vybraná slova dohromady pomocí znaménka plus (+). 
+# Přidáváme mezi ně i prázdné mezery v uvozovkách (" "), aby slova nebyla nalepená na sobě.
+pribeh = vybrana_postava + " " + vybrane_misto + " " + vybrany_dej
+
+# Vypíšeme výsledný příběh do konzole (na obrazovku)
+print(pribeh)
