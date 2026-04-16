61d73f08dca25e6e682c8195df20a5b1dc8de847
michal12346 <michal.balon@tria-tr.cz>
2026-04-16 20:57:34 +0200

    Přidána nekonečná smyčka pro neustálou aktualizaci času každou vteřinu.

--- Hodiny/Hodiny.py
+++ Hodiny/Hodiny.py
@@ -1,11 +1,17 @@
-# Hodiny - PRVNÍ VERZE
+# Hodiny - DRUHÁ VERZE
 
-# Importujeme knihovnu time, která v Pythonu slouží k práci s časem
+# Importujeme knihovnu time pro práci s časem a pozastavení programu
 import time
 
-# Získáme aktuální čas a zformátujeme ho
-# %H znamená hodiny, %M minuty a %S sekundy
-aktualni_cas = time.strftime("%H:%M:%S")
-
-# Jednoduše vypíšeme získaný čas do konzole
-print("Aktuální čas:", aktualni_cas)
+# Začátek logicky složitější části: Nekonečná smyčka 'while'
+# Podmínka 'True' znamená, že smyčka poběží donekonečna, dokud program natvrdo nevypneme
+while True:
+    # Získáme aktuální čas v každém průběhu smyčky
+    aktualni_cas = time.strftime("%H:%M:%S")
+    
+    # Vypíšeme nový čas na nový řádek v konzoli
+    print("Aktuální čas:", aktualni_cas)
+    
+    # Funkce sleep() uspí program přesně na 1 vteřinu
+    # Děláme to proto, aby nám to nezahltilo počítač a čas se měnil po vteřinách
+    time.sleep(1)
